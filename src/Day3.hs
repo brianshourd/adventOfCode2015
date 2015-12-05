e9122e0543a61797d7434a06c143b23dd6ba5527
Brian Shourd <brian.shourd@gmail.com>
2015-12-04 17:03:50 -0800

    Day 3 code (parts 1 and 2) complete

---
+++ src/Day3.hs
@@ -0,0 +1,36 @@
+module Day3 (day3, day3') where
+
+import Data.Set (Set, insert, empty, size, union)
+
+-- Given a string such as "><<>", return the number of houses visited
+day3 :: String -> Int
+day3 = size . snd . foldl move ((0, 0), insert (0, 0) empty)
+
+type Position = (Integer, Integer)
+type State = (Position, Set Position)
+
+move :: State -> Char -> State
+move (p, ps) dir = let q = go dir p in (q, insert q ps)
+
+go :: Char -> Position -> Position
+go dir (x, y) = case dir of
+    '>' -> (x + 1, y)
+    '<' -> (x - 1, y)
+    '^' -> (x, y + 1)
+    'v' -> (x, y - 1)
+    _   -> (x, y)
+
+
+-- Second half
+day3' :: String -> Int
+day3' = size . merge . foldl move' (startState, startState)
+    where
+        startState = ((0, 0), insert (0, 0) empty)
+        merge :: TwoState -> Set Position
+        merge ((_, ps), (_, qs)) = union ps qs
+
+type TwoState = (State, State)
+
+move' :: TwoState -> Char -> TwoState
+move' ((p, ps), other) dir = let q = go dir p in (other, (q, insert q ps))
+
---
+++ test/Day3Spec.hs
@@ -0,0 +1,29 @@
+module Day3Spec (spec) where
+
+import Day3
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "day3" $ do
+    it "delivers to 2 houses on '>'" $ do
+      day3 ">" == 2
+
+    it "delivers to 4 houses on '^>v<'" $
+      day3 "^>v<" == 4
+
+    it "delivers to 2 houses on '^v^v^v^v^v'" $
+      day3 "^v^v^v^v^v" == 2
+
+  describe "day3'" $ do
+    it "delivers to 3 houses on '^v'" $ do
+      day3' "^v" == 3
+
+    it "delivers to 3 houses on '^>v<'" $
+      day3' "^>v<" == 3
+
+    it "delivers to 11 houses on '^v^v^v^v^v'" $
+      day3' "^v^v^v^v^v" == 11
