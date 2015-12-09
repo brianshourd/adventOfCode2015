057a74102399f23d8523876a16951d8388d1f1fa
Brian Shourd <brian.shourd@gmail.com>
2015-12-09 00:09:05 -0800

    Day 7 code (part 1) complete

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,11 +1,136 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day7 (day7, day7', run) where
 
 import Control.Applicative
-import Data.Attoparsec.ByteString.Char8
-import Data.ByteString.Char8 (pack)
+import Control.Monad (liftM2)
+import Data.Attoparsec.Text
+import Data.Bits
+import Data.Function (fix)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import qualified Data.Map.Lazy as ML
+import Data.Set (Set)
+import Data.Text (Text, pack, unpack)
 
+-- The function here is two parts:
+-- 1. parseInstructions - Parse the text into a dictionary of Instructions,
+-- whose keys are References to wires and whose values are the associated
+-- Expressions
+-- 2. resolve - resolves a set of Instructions and a Reference to a wire to a
+-- final signal value on that wire
+-- The resolve function uses resolve' (which performs map lookups), and evaluate
+-- (which performs and/or/shift logic) co-recursively, resolving and memoizing
+-- wire references as it goes
 day7 :: String -> Int
-day7 = undefined
+day7 input = case parseAndApply input of
+    (Right i) -> i
+    _         -> -1
+
+parseAndApply :: String -> Either String Int
+parseAndApply input = do
+    instructions <- parseOnly parseInstructions (pack input)
+    resolve instructions "a"
+
+-- Begin section: parsing instructions into a Map (dictionary)
+data Expression =
+    JunctionEx JunctionOp Expression Expression |
+    ShiftEx ShiftOp Expression Int |
+    NotEx Expression |
+    ReferenceEx Reference |
+    SignalEx Int
+    deriving Show
+data JunctionOp = AndOp | OrOp deriving Show
+data ShiftOp = LShiftOp | RShiftOp deriving Show
+type Reference = Text
+type Instruction = (Reference, Expression)
+type Instructions = Map Reference Expression
+
+parseInstructions :: Parser Instructions
+parseInstructions = fmap M.fromList . many' $ parseInstruction <* option () endOfLine
+
+parseInstruction :: Parser Instruction
+parseInstruction = do
+    e <- parseExpression
+    string " -> "
+    wireLabel <- takeTill isEndOfLine
+    return (wireLabel, e)
+
+parseExpression :: Parser Expression
+parseExpression = choice [parseJunctionEx, parseShiftEx, parseNotEx, parseSignalOrReference]
+
+parseSignalOrReference :: Parser Expression
+parseSignalOrReference = choice [parseSignalEx, parseReferenceEx]
+
+parseJunctionEx :: Parser Expression
+parseJunctionEx = do
+    a <- parseSignalOrReference
+    char ' '
+    op <- (string "AND" >> return AndOp) <|> (string "OR"  >> return OrOp)
+    char ' '
+    b <- parseSignalOrReference
+    return $ JunctionEx op a b
+
+parseShiftEx :: Parser Expression
+parseShiftEx = do
+    a <- parseSignalOrReference
+    char ' '
+    op <- (string "LSHIFT" >> return LShiftOp) <|> (string "RSHIFT" >> return RShiftOp)
+    char ' '
+    b <- decimal
+    return $ ShiftEx op a b
+
+parseNotEx :: Parser Expression
+parseNotEx = do
+    string "NOT "
+    a <- parseSignalOrReference
+    return $ NotEx a
+
+parseReferenceEx :: Parser Expression
+parseReferenceEx = do
+    r <- parseReference
+    return $ ReferenceEx r
+
+parseSignalEx :: Parser Expression
+parseSignalEx = do
+    s <- decimal
+    return $ SignalEx s
+
+parseReference :: Parser Reference
+parseReference = takeTill isHorizontalSpace
+-- End section on parsing
+
+-- The meat of the actual algorithm starts here
+-- evaluate is responsible for evaluating Expressions. Given a function for
+-- resolving References and an Expression (which might contain References), it
+-- performs the logic to request Reference resolution and combine the results.
+evaluate :: (Reference -> Either String Int) -> Expression -> Either String Int
+evaluate res (SignalEx s) = Right s
+evaluate res (ReferenceEx r) = res r
+evaluate res (NotEx ex) = fmap complement (evaluate res ex)
+evaluate res (ShiftEx op ex x) = fmap shiftFunc (evaluate res ex)
+    where
+        shiftFunc = case op of
+            LShiftOp -> flip shiftL x
+            RShiftOp -> flip shiftR x
+evaluate res (JunctionEx op ex1 ex2) = liftM2 junctionFunc (evaluate res ex1) (evaluate res ex2)
+    where
+        junctionFunc = case op of
+            AndOp -> (.&.)
+            OrOp  -> (.|.)
+
+-- resolve takes a list of instructions and a reference to a wire (e.g. "aq")
+-- and returns the signal on that wire. Intermediate calculations are memoized,
+-- so that the calculation actually finishes.
+resolve :: Instructions -> Reference -> Either String Int
+resolve store = fix $ memoize (M.keysSet store) . resolve' store
+
+memoize :: Set Reference -> (Reference -> a) -> Reference -> a
+memoize keys f key = (ML.fromSet f keys) ML.! key
+
+resolve' :: Instructions -> (Reference -> Either String Int) -> Reference -> Either String Int
+resolve' store f ref = case M.lookup ref store of
+    (Just ex) -> evaluate f ex
+    Nothing   -> Left $ "Failed to resolve " ++ (unpack ref)
 
 day7' :: String -> Int
 day7' = undefined
@@ -14,7 +139,6 @@ day7' = undefined
 run :: IO ()
 run = do
     putStrLn "Day 7 results: "
-    putStrLn "  In progress"
-    --input <- readFile "inputs/day7.txt"
-    --putStrLn $ "  " ++ show (day7 input)
+    input <- readFile "inputs/day7.txt"
+    putStrLn $ "  " ++ show (day7 input)
     --putStrLn $ "  " ++ show (day7' input)
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -1,5 +1,6 @@
 module Day7Spec (spec) where
 
+import Data.Bits
 import Day7
 import Test.Hspec
 
@@ -9,8 +10,33 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "day7" $ do
-    it "is defined" $ do
-      day7 "something" /= undefined
+    it "123 -> x means that the signal 123 is provided to wire x." $ do
+      day7 "123 -> a" == 123
+
+    it "123 -> y then y -> x means that the signal 123 is provided to wire x." $ do
+      day7 (unlines ["123 -> y", "y -> a"]) == 123
+
+    it "x AND y -> z means that the bitwise AND of wire x and wire y is provided to wire z." $ do
+      day7 (unlines ["123 -> x", "456 -> y", "x AND y -> a"]) == 123 .&. 456
+
+    it "1 AND y -> z means that the bitwise AND of 1 and wire y is provided to wire z." $ do
+      day7 (unlines ["456 -> y", "1 AND y -> a"]) == 1 .&. 456
+
+    it "p LSHIFT 2 -> q means that the value from wire p is left-shifted by 2 and then provided to wire q." $ do
+      day7 (unlines ["123 -> p", "p LSHIFT 2 -> a"]) == shiftL 123 2
+
+    it "NOT e -> f means that the bitwise complement of the value from wire e is provided to wire f." $ do
+      day7 (unlines ["123 -> e", "NOT e -> a"]) == complement 123
+
+    it "NOT e -> f means that the bitwise complement of the value from wire e is provided to wire f." $ do
+      day7 (unlines
+        [ "1 -> b"
+        , "1 -> c"
+        , "c RSHIFT 2 -> d"
+        , "d AND c -> e"
+        , "e OR b -> f"
+        , "NOT f -> a"
+        ]) == complement 1
 
   describe "day7'" $ do
     it "is defined" $ do
