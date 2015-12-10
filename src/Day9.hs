a3d4ba56c2c173aef7d47e48dcac4db2d7934ed8
Brian Shourd <brian.shourd@gmail.com>
2015-12-09 21:48:44 -0800

    Day 9 parsing complete

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,11 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Day9 (day9, day9', run) where
+module Day9 (day9, day9', run, day9Test) where
 
 import Data.Attoparsec.Text
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Text (Text, pack, unpack)
 
 day9 :: String -> Int
 day9 = undefined
+
+day9Test :: String -> Either String SourceData
+day9Test = parseOnly parseSourceData . pack
+
+type City = Text
+data Source = Source
+    { start    :: City
+    , end      :: City
+    , distance :: Int
+    } deriving Show
+type Route = [City]
+type SourceData = Map City [(City, Int)]
+
+compileMap :: [Source] -> SourceData
+compileMap sources = M.fromSet (getAllConnections allSources) cities
+    where
+        cities = S.fromList . map start $ allSources
+        allSources = sources ++ (map (\(Source a b d) -> Source b a d) sources)
+
+getAllConnections :: [Source] -> City -> [(City, Int)]
+getAllConnections sources city = map (\s -> (end s, distance s))
+    . filter (\s -> city == (start s))
+    $ sources
+
+parseSourceData :: Parser SourceData
+parseSourceData = do
+    sources <- many' parseSource
+    return $ compileMap sources
+
+parseSource :: Parser Source
+parseSource = do
+    a <- takeTill isHorizontalSpace
+    string " to "
+    b <- takeTill isHorizontalSpace
+    string " = "
+    dist <- decimal
+    endOfLine
+    return $ Source a b dist
+
+shortestPath :: Set City -> (Route, Int)
+shortestPath = undefined
+
+shortestPaths :: Set City -> [(Route, Int)]
+shortestPaths cities = undefined
 
 day9' :: String -> Int
 day9' = undefined
