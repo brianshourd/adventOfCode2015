765b7ae7b4a203c0f2fc5c31abdbd9be97045eae
Brian Shourd <brian.shourd@gmail.com>
2015-12-22 22:11:13 -0800

    Day 9 (part 2) complete, plus refactor to generic graph traversals

--- src/Day9.hs
+++ src/Day9.hs
@@ -2,40 +2,103 @@
 module Day9 (day9, day9', run) where
 
 import Data.Attoparsec.Text
-import Data.List (minimumBy)
+import Data.Function (on)
+import Data.List (maximumBy, minimumBy, sort)
 import Data.Maybe (catMaybes)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import Data.Monoid (Sum(..))
 import Data.Ord (comparing)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Data.Text (Text, pack, unpack)
 import Safe
 
-day9 :: String -> Int
-day9 input = case parseOnly parseSourceData . pack $ input of
-    (Left _) -> -1
-    (Right sourceData) -> minimum . map snd . shortestRoutes sourceData $ (allCities sourceData)
+-- Defining data types for an undirected, weighted graph, with the additional
+-- constraint that there are no lone nodes, so that the entirety of the graph
+-- can be represented by the Edge map
+-- Expectations: Ord t
+data Node t = Node t deriving (Show, Eq, Ord)
+data Edge t = Edge
+    { edgeStart :: Node t
+    , edgeEnd   :: Node t
+    } deriving Show
+-- Undirected edges
+instance Ord t => Eq (Edge t) where
+    (Edge s1 e1) == (Edge s2 e2) = sort [s1, e1] == sort [s2, e2]
+instance Ord t => Ord (Edge t) where
+    compare (Edge s1 e1) (Edge s2 e2) = compare (sort [s1, e1]) (sort [s2, e2])
+type EdgeWeightMap t w = Map (Edge t) w
+type NodeSet t = Set (Node t)
+data Traversal t w = Traversal
+    { traversalNodes  :: [Node t]
+    , traversalWeight :: w
+    }
 
+-- Basic graph functionality
+nodes :: Ord t => EdgeWeightMap t w -> NodeSet t
+nodes = S.fromList . concat . map (\(Edge s1 e1) -> [s1, e1]) . M.keys
+
+-- Prepend a node to a traversal, increasing the weight
+prependNode :: (Ord t, Monoid w)
+    => EdgeWeightMap t w
+    -> Node t
+    -> Traversal t w
+    -> Maybe (Traversal t w)
+prependNode g n (Traversal [] w)     = Just $ Traversal [n] w
+prependNode g n (Traversal (x:xs) w) =
+    fmap (Traversal (n:x:xs) . mappend w) $ M.lookup (Edge x n) g
+
+-- Find the best complete traversal, if one exists
+bestTraversal :: (Ord t, Monoid w)
+    => EdgeWeightMap t w    -- source data
+    -> (w -> w -> Ordering) -- comparison function
+    -> Maybe (Traversal t w)
+bestTraversal sourceData comp =
+    maximumByMay (comp `on` traversalWeight) . bestTraversals sourceData comp $ nodes sourceData
+
+-- Find the best traversal that starts at the given node and visits every one of
+-- the other nodes provided, if one exists
+bestTraversal' :: (Ord t, Monoid w)
+    => EdgeWeightMap t w    -- source data
+    -> (w -> w -> Ordering) -- comparison function
+    -> Node t               -- start node
+    -> NodeSet t            -- other nodes to visit
+    -> Maybe (Traversal t w)
+bestTraversal' sourceData comp s ns =
+    maximumByMay (comp `on` traversalWeight)
+    . catMaybes
+    . map (prependNode sourceData s)
+    $ bestTraversals sourceData comp ns
+
+-- Find the best complete traversals for just the nodes provided, one for each
+-- starting node
+bestTraversals :: (Ord t, Monoid w)
+    => EdgeWeightMap t w    -- source data
+    -> (w -> w -> Ordering) -- comparison function
+    -> NodeSet t            -- nodes to visit
+    -> [Traversal t w]
+bestTraversals sourceData comp ns =
+    if null ns
+    then [Traversal [] mempty]
+    else catMaybes . map bestFrom . S.elems $ ns
+    where
+        bestFrom n = bestTraversal' sourceData comp n (S.delete n ns)
+
+-- Parsing types and type synonyms for the problem at hand
 type City = Text
+type Distance = Sum Int
 data Source = Source
     { start    :: City
     , end      :: City
-    , distance :: Int
+    , distance :: Distance
     } deriving Show
-type Route = [City]
-type SourceData = Map City [(City, Int)]
+type SourceData = EdgeWeightMap City Distance
 
 compileMap :: [Source] -> SourceData
-compileMap sources = M.fromSet (getAllConnections allSources) cities
+compileMap = M.fromList . map toPair
     where
-        cities = S.fromList . map start $ allSources
-        allSources = sources ++ (map (\(Source a b d) -> Source b a d) sources)
-
-getAllConnections :: [Source] -> City -> [(City, Int)]
-getAllConnections sources city = map (\s -> (end s, distance s))
-    . filter (\s -> city == (start s))
-    $ sources
+        toPair (Source s e d) = (Edge (Node s) (Node e), d)
 
 parseSourceData :: Parser SourceData
 parseSourceData = do
@@ -50,32 +113,21 @@ parseSource = do
     string " = "
     dist <- decimal
     endOfLine
-    return $ Source a b dist
+    return $ Source a b (Sum dist)
 
-allCities :: SourceData -> Set City
-allCities = S.fromList . M.keys
-
--- The shortest route starting here and visiting the provided cities
-shortestRoute :: SourceData -> City -> Set City -> Maybe (Route, Int)
-shortestRoute sourceData start rest = if (null rest)
-    then Just ([start], 0)
-    else minimumByMay (comparing snd)
-        . catMaybes
-        . map (prependRoute sourceData start)
-        $ shortestRoutes sourceData rest
-
-prependRoute :: SourceData -> City -> (Route, Int) -> Maybe (Route, Int)
-prependRoute sourceData x (ys, dst) = do
-    firstCity <- headMay ys
-    distances <- M.lookup x sourceData
-    distanceToFirst <- lookup firstCity distances
-    return (x:ys, dst + distanceToFirst)
-
-shortestRoutes :: SourceData -> Set City -> [(Route, Int)]
-shortestRoutes sourceData cities = catMaybes . map (\city -> shortestRoute sourceData city (S.delete city cities)) $ S.elems cities
+-- Begin actual implementation
+day9 :: String -> Int
+day9 = day9Impl (compare `on` negate)
 
 day9' :: String -> Int
-day9' = undefined
+day9' = day9Impl compare
+
+day9Impl :: (Sum Int -> Sum Int -> Ordering) -> String -> Int
+day9Impl comp input = case parseOnly parseSourceData . pack $ input of
+    (Left _) -> -1
+    (Right sourceData) -> case (bestTraversal sourceData comp) of
+        Nothing -> -2
+        (Just t) -> getSum . traversalWeight $ t
 
 -- Input
 run :: IO ()
@@ -83,4 +135,4 @@ run = do
     putStrLn "Day 9 results: "
     input <- readFile "inputs/day9.txt"
     putStrLn $ "  " ++ show (day9 input)
-    --putStrLn $ "  " ++ show (day9' input)
+    putStrLn $ "  " ++ show (day9' input)
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -18,5 +18,9 @@ spec = do
         ]) == 605
 
   describe "day9'" $ do
-    it "is defined" $ do
-      day9' "" /= undefined
+    it "Given the following distances:\nLondon to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141\nThe *longest* possible route is Dublin -> London -> Belfast = 982." $ do
+      day9' (unlines
+        [ "London to Dublin = 464"
+        , "London to Belfast = 518"
+        , "Dublin to Belfast = 141"
+        ]) == 982
