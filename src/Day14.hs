89e2b06418e3e7bf8fbf60464056636640b53d31
Brian Shourd <brian.shourd@gmail.com>
2015-12-26 11:58:41 -0800

    Day 14 (parts 1 and 2) complete

--- src/Day14.hs
+++ src/Day14.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Day14 (day14, day14', run, Reindeer(..), leadReindeer, parseInput) where
+module Day14 (day14, day14', run, Reindeer(..), leadReindeer, parseInput, pointResults) where
 
+import Control.Applicative (optional)
 import Data.Attoparsec.Text
     ( Parser(..)
-    , char
     , decimal
     , endOfLine
     , isHorizontalSpace
     , many'
-    , option
     , parseOnly
     , string
     , takeTill
     )
+import Data.List (transpose)
+import Data.Text (Text, pack)
 
 data Reindeer = Reindeer
-    !String  -- Name
-    !Int     -- Speed
-    !Int     -- Flying time
-    !Int     -- Resting time
+    !Text  -- Name
+    !Int   -- Speed
+    !Int   -- Flying time
+    !Int   -- Resting time
     deriving (Show, Eq)
 
 reindeerName (Reindeer n _ _ _) = n
@@ -26,17 +27,74 @@ reindeerSpeed (Reindeer _ s _ _) = s
 reindeerFlyTime (Reindeer _ _ f _) = f
 reindeerRestTime (Reindeer _ _ _ r) = r
 
+parseReindeer :: Parser Reindeer
+parseReindeer = do
+    name <- takeTill isHorizontalSpace
+    string " can fly "
+    speed <- decimal
+    string " km/s for "
+    flyTime <- decimal
+    string " seconds, but then must rest for "
+    restTime <- decimal
+    string " seconds."
+    optional endOfLine
+    return $ Reindeer name speed flyTime restTime
+
 parseInput :: String -> Either String [Reindeer]
-parseInput = undefined
+parseInput = parseOnly (many' parseReindeer) . pack
 
 leadReindeer :: [Reindeer] -> Int -> Int
-leadReindeer = undefined
+leadReindeer rs t = maximum . map (runFor t) $ rs
+
+runFor
+    :: Int       -- How many seconds
+    -> Reindeer  -- Reindeer
+    -> Int       -- Distance travelled
+runFor t (Reindeer _ speed flyTime restTime) =
+    snd . head . filter ((== 0) . fst) $ iterate cycle (t, 0)
+  where
+    cycle = (go 0 restTime) . (go speed flyTime)
+    go rate time (secondsLeft, dist)
+        | secondsLeft <= time = (0, secondsLeft * rate + dist)
+        | otherwise           = (secondsLeft - time, time * rate + dist)
 
 day14 :: String -> Int
-day14 = undefined
+day14 input = case parseInput input of
+    (Left _)   -> -1
+    (Right rs) -> leadReindeer rs 2503
+
+data State = State
+    { reindeer :: Reindeer
+    , distance :: Int
+    , points   :: Int
+    }
+
+pointResults :: [Reindeer] -> Int -> [Int]
+pointResults rs t =
+    map sum
+    . transpose
+    . map toPoints
+    . transpose
+    . map rPositions
+    $ rs
+  where
+    rPositions :: Reindeer -> [Int]
+    rPositions (Reindeer _ speed flyTime restTime) =
+        take t
+        . tail
+        . scanl (+) 0
+        . concat
+        . repeat
+        $ concat [take flyTime $ repeat speed, take restTime $ repeat 0]
+    toPoints :: [Int] -> [Int]
+    toPoints positions = map (\p -> if p == maxPos then 1 else 0) positions
+      where
+        maxPos = maximum positions
 
 day14' :: String -> Int
-day14' = undefined
+day14' input = case parseInput input of
+    (Left _)   -> -1
+    (Right rs) -> maximum $ pointResults rs 2503
 
 -- Input
 run :: IO ()
--- test/Day14Spec.hs
+++ test/Day14Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Day14Spec (spec) where
 
 import Day14
@@ -6,26 +7,30 @@ import Test.Hspec
 main :: IO ()
 main = hspec spec
 
-input :: String
-input = unlines
+rawInput :: String
+rawInput = unlines
     [ "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
     , "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+    ]
+
+parsedInput :: [Reindeer]
+parsedInput =
+    [ Reindeer "Comet" 14 10 127
+    , Reindeer "Dancer" 16 11 162
     ]
 
 spec :: Spec
 spec = do
     describe "day14 parser" $ do
         it "Parses the provided input properly" $ do
-            parseInput input `shouldBe` Right
-                [ Reindeer "Comet" 14 10 127
-                , Reindeer "Dancer" 16 11 162
-                ]
+            parseInput rawInput `shouldBe` Right parsedInput
 
     describe "day14" $ do
         it "Given the input, after 1000 seconds, Comet is in the lead at 1120 km" $ do
-            leadReindeer rs 1000 `shouldBe` 1120
-              where
-                rs =
-                    [ Reindeer "Comet" 14 10 127
-                    , Reindeer "Dancer" 16 11 162
-                    ]
+            leadReindeer parsedInput 1000 `shouldBe` 1120
+
+    describe "day14'" $ do
+        it "Given the input, after 1000 seconds, Comet has 312 points and Dancer has 689 points" $ do
+            let results = pointResults parsedInput 1000
+            results !! 0 `shouldBe` 312
+            results !! 1 `shouldBe` 689
